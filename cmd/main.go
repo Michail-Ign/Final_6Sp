c92bceb16671cef444c1706d89b460ac6ed82dbc
Michail Ignatov <ignatov.mu@yandex.ru>
2025-03-30 20:19:23 +0300

    Завершение работы.30-03-25 20:19

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -15,12 +16,13 @@ func main() {
 
 	// 2 -  создать сервер с помощью вашей функции из пакета server, и запустить его.
 	myserver := server.CreateRouter(errorLog)
+
 	// запускаем сервер
 	if err := http.ListenAndServe(myserver.Addr, myserver.Handler); err != nil {
-		// 3-  при запуске сервера возникают ошибки, выведите её с помощью логгера на уровне Fatal
 
-		errorLog.Fatal("Ошибка при запуске сервера: %s", err.Error())
+		// 3-  при запуске сервера возникают ошибки, выведите её с помощью логгера на уровне Fatal
+		str := fmt.Sprintf("ошибка при запуске сервера: %v", err)
+		errorLog.Fatal(str)
 		return
 	}
-
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,11 +5,11 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 	"text/template"
 	"time"
 
 	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
-	"github.com/go-delve/delve/service"
 )
 
 var tpl = template.Must(template.ParseFiles("index.html"))
@@ -26,52 +26,50 @@ func HandleUpload(w http.ResponseWriter, r *http.Request) {
 	//Получение файла из формы
 	myfile, _, err := r.FormFile("myFile")
 	if err != nil {
-		http.Error(w, "ошибка при получении файла", http.StatusInternalServerError)
+
+		str := fmt.Sprintf("ошибка(%v) при получении файла", err)
+		http.Error(w, str, http.StatusInternalServerError)
+
 		return
 	}
 	defer myfile.Close()
 
-	// создаем мапу для хранения преобразованных строк из файла
 	str_res := ""
 
-	// создаем сканер для построчного чтения файла
 	scanner := bufio.NewScanner(myfile)
 
-	// читаем файл построчно
 	for scanner.Scan() {
 		line := scanner.Text()
+
 		if line == "" {
 			continue
 		}
 		//Переконвертация каждой строки файла и зпоминае в мапе
 		str_res = str_res + service.ToConvert(line) + "\n"
-		// декодируем JSON-строку в структуру LogEntry
-
-		//if err != nil {
-		//	log.Printf("ошибка парсинга: %v. Строка: %s\n", err, line)
-		//	continue
-		//}
-		//i++
 	}
 
 	// создаём локальный файл для записи данных
-	name_file := time.Now().UTC().String()
+	name_file := time.Now().UTC().String() + ".txt"
+	name_file = strings.Replace(name_file, ":", "_", -1)
+
 	resFile, err := os.Create(name_file)
 	if err != nil {
-		//log.Fatal(err)
-		http.Error(w, "ошибка при cоздании локального файла", http.StatusInternalServerError)
+
+		str := fmt.Sprintf("ошибка(%v) при cоздании локального файла", err)
+		http.Error(w, str, http.StatusInternalServerError)
+
 		return
 	}
 	defer resFile.Close()
 
-	//for line_w, _ := range map_data {
 	_, err = fmt.Fprintf(resFile, "%s", str_res)
 	if err != nil {
-		//log.Fatal(err)
-		http.Error(w, "ошибка при сохранении результата в локальный файл", http.StatusInternalServerError)
+
+		str := fmt.Sprintf("ошибка(%v) при сохранении результата в локальный файл", err)
+		http.Error(w, str, http.StatusInternalServerError)
+
 		return
 	}
-	//}
 
 	w.Header().Set("Content-Type", "text/plain")
 	w.Write([]byte(str_res))
